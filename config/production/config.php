173cdd0e8b60889a502a61ed65376b8a25e0caa3
Diego Torres <diegotorres50@hotmail.com>
2017-05-21 20:31:45 -0300

    Prod config has been added

--- config/production/config.php
+++ config/production/config.php
@@ -5,8 +5,8 @@ return [
             'mysqli' => [
                 'mysql_server' => 'localhost',
                 'mysql_user' => 'diego_torres',
-                'mysql_password' => 'ku@leezof+2017',
-                'mysql_database' => 'diegotor_qualisoft_prod',
+                'mysql_password' => 'toolbox2017',
+                'mysql_database' => 'diegotor_toolbox_prod',
                 'mysql_port'     => 3306
             ]
         ],
